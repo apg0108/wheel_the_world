6d7ecbdc9379bad9e38cf67c8b22907a1e5104aa
Asiel <asiel0108@gmail.com>
2021-09-25 12:34:11 -0400

    Minor Fixes

--- src/Excercise3.ts
+++ src/Excercise3.ts
@@ -1,5 +1,7 @@
 function BigZ(data: number[][]): number[] {
     let arr: number[] = [];
+    let index = 0;
+    let j = 0;
     if (data.length === 0)
         return [];
     if (data.length === 1)
@@ -7,32 +9,37 @@ function BigZ(data: number[][]): number[] {
     arr.push(data[0][0], data[1][0]);
     data[0][0] = null;
     data[1][0] = null;
-    for (let index = 0; index < data.length; index++)
-        for (let j = 1; j < data[index].length; j++) {
+    while (index < data.length) {
+        j = 0;
+        while (j < data[index].length) {
             if (data[index][j]) {
                 arr.push(data[index][j]);
                 data[index][j] = null;
-            }            
+            }
             if (data[index + 1] && data[index + 1][j - 1]) {
                 arr.push(data[index + 1][j - 1]);
                 data[++index][--j] = null;
             } else {
-                if (data[index + 1] && data[index][j + 1]) {
+                /*  if (data[index + 1] && data[index][j + 1]) {
+                     arr.push(data[index][++j]);
+                     data[index][j] = null;
+                     if (data[index - 1] && data[index - 1][j + 1]) {
+                         arr.push(data[--index][++j]);
+                         data[index][j] = null;
+                     }
+                 } else */
+                if (data[index - 1] && data[index - 1][j + 1]) {
+                    arr.push(data[--index][++j]);
+                    data[index][j] = null;
+                } else if (data[index] && data[index][j + 1]) {
                     arr.push(data[index][++j]);
                     data[index][j] = null;
-                    if (data[index - 1] && data[index - 1][j + 1]) {
-                        arr.push(data[--index][++j]);
-                        data[index][j] = null;
-                    }
-                } else if (!data[index + 1]) {
-                    if (data[index - 1] && data[index - 1][j + 1]){
-                        arr.push(data[--index][++j]);
-                        data[index][j] = null;
-                    }
-                }
+                } else j++;
             }
         }
+        index++;
+    }
     return arr;
 }
 
-//console.log(BigZ([[1, 2], [4,5,6], [7,8,9]]));
+//console.log(BigZ([[1, 2], [4, 5], [7, 8, 9,12]]));
